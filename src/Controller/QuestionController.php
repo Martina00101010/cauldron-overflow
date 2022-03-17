5e1d541e9e40c1dcec744e6d6c682fc8d12f0b04
a.mukalled <a.mukalled@rabota.ru>
2022-03-18 01:53:27 +0300

    Chapter 3: homepage controller & route

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class QuestionController
+{
+    public function homepage(): Response
+    {
+        return new Response("Ask your question!");
+    }
+}
