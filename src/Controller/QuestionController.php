27c24d2531e1dede39ed8529c8f0a5acfa8e6067
a.mukalled <a.mukalled@rabota.ru>
2022-03-20 19:30:53 +0300

    Chapter 9: twig template, try to go to https://127.0.0.1:8000/question/what-interesting-facts-have-you-heard

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     public function homepage(): Response
     {
@@ -13,9 +14,19 @@ class QuestionController
 
     public function show($question): Response
     {
-        return new Response(
-            ucwords(str_replace('-', ' ', $question))
-            . "? There could be your answer"
+        $answers = [
+            "Theoretically, it would take just 45 hours on a Boeing 747 to fly around the Earth's circumference",
+            "The sun is white. It looks yellow because the Earth's atmosphere scatters blue light, \
+            so that the Sun colour is white minus blue, i.e. red plus green = yellow",
+            "Many species of trees that grow near the equator can grow several meters per year",
+            "Virtual reality and Augmented reality are ones of the most evolving technologies"
+        ];
+        return $this->render(
+            'question/show.html.twig',
+            [
+                'question' => ucwords(str_replace('-', ' ', $question)) . '?',
+                'answers' => $answers
+            ]
         );
     }
 }
