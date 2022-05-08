0923d38c7aa8d1ed41b75664fcd07f737c7e0cca
a.mukalled <a.mukalled@rabota.ru>
2022-05-08 21:22:54 +0300

    Chapter 2.2: markdown 2 ways: with knp-bundle directly and from twig

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -18,20 +19,22 @@ class QuestionController extends AbstractController
         // return (new Response($htmlPage));
     }
 
-    public function show($question): Response
+    public function show($question, MarkdownParserInterface $markdownParser): Response
     {
         $answers = [
-            "Theoretically, it would take just 45 hours on a Boeing 747 to fly around the Earth's circumference",
+            "Theoretically, it would take `just 45 hours` on a Boeing 747 to fly around the Earth's circumference",
             "The sun is white. It looks yellow because the Earth's atmosphere scatters blue light, 
             so that the Sun colour is white minus blue, i.e. red plus green = yellow",
             "Many species of trees that grow near the equator can grow several meters per year",
             "Virtual reality and Augmented reality are ones of the most evolving technologies"
         ];
-        dump($answers, $this); // to remember how to use dump
+//        dump($answers, $this); // to remember how to use dump
+        $question = "What interesting facts **astonished** you most?";
+        $parsedQuestion = $markdownParser->transformMarkdown($question);
         return $this->render(
             'question/show.html.twig',
             [
-                'question' => ucwords(str_replace('-', ' ', $question)) . '?',
+                'question' => ucwords(str_replace('-', ' ', $parsedQuestion)) . '?',
                 'answers' => $answers
             ]
         );
